a81709a6207f3412a4f6ec8169de5ca10cc44850
nnorwitz <nnorwitz@806ff5bb-693f-0410-b502-81bc3482ff28>
2007-12-08 23:51:26 +0000

    Some sample "visitors".
    git-svn-id: http://cppclean.googlecode.com/svn/trunk@9 806ff5bb-693f-0410-b502-81bc3482ff28

---
+++ headers/cpp/functions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""Find and print the functions in a source file."""
+
+import os
+import sys
+
+from cpp import ast
+from cpp import utils
+
+
+def PrintFunctions(filename):
+    source = utils.ReadFile(filename, False)
+    if source is None:
+        print 'Unable to find', filename
+        return
+
+    #print 'Processing', actual_filename
+    builder = ast.BuilderFromSource(source)
+    try:
+        for node in builder.Generate():
+            # TODO(nnorwitz): need to ignore friend method declarations.
+            if isinstance(node, ast.Function):
+                print node.name
+    except:
+        pass
+
+    
+def main(argv):
+    for filename in argv[1:]:
+        PrintFunctions(filename)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ headers/cpp/methods.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""Find and print the methods in a source file."""
+
+import os
+import sys
+
+from cpp import ast
+from cpp import utils
+
+
+def PrintMethods(filename):
+    source = utils.ReadFile(filename, False)
+    if source is None:
+        print 'Unable to find', filename
+        return
+
+    #print 'Processing', actual_filename
+    builder = ast.BuilderFromSource(source)
+    try:
+        # TODO(nnorwitz): also need to ignore friend methods.
+        special_function = ast.FUNCTION_CTOR | ast.FUNCTION_DTOR
+        for node in builder.Generate():
+            if isinstance(node, ast.Class) and node.body:
+                for node in node.body:
+                    if (isinstance(node, ast.Function) and
+                        not (node.modifiers & special_function))::
+                        print node.name
+    except:
+        pass
+
+    
+def main(argv):
+    for filename in argv[1:]:
+        PrintMethods(filename)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
