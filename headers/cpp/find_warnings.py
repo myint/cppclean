b5417bec4ee50d5e157c530957164fceacb646c8
nnorwitz <nnorwitz@806ff5bb-693f-0410-b502-81bc3482ff28>
2008-03-15 15:09:49 +0000

    Mark all headers as required for parameter types when:  * the function has an implementation AND  * the parameter has a name (thus could be used) For functions without an implementation, the normal mechanism is used (ie, ptr/ref only needs forward decl)
    git-svn-id: http://cppclean.googlecode.com/svn/trunk@199 806ff5bb-693f-0410-b502-81bc3482ff28

--- headers/cpp/find_warnings.py
+++ headers/cpp/find_warnings.py
@@ -250,7 +250,15 @@ class WarningHunter(object):
             templated_types = function.templated_types or ()
             for p in ast._SequenceToParameters(function.parameters):
                 if p.type_name not in templated_types:
-                    _AddVariable(p, p.type_name, function.namespace)
+                    if function.body and p.name and p.type_name:
+                        # Assume that if the the function has a body and a name
+                        # the parameter type is really used.
+                        # NOTE(nnorwitz): this is over-aggressive. It would be
+                        # better to iterate through the body and determine
+                        # actual uses based on local vars and data members used.
+                        _AddUse(p.type_name, function.namespace)
+                    else:
+                        _AddVariable(p, p.type_name, function.namespace)
             if function.body:
                 # TODO(nnorwitz): handle local vars and data member references.
                 pass
