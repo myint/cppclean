ec24f4e94087bef3fdd896c52cf605dbf95d2fd4
Steven Myint <git@stevenmyint.com>
2013-10-05 09:55:54 -0700

    Do things properly by not shoe horning things in

--- cpp/static_data.py
+++ cpp/static_data.py
@@ -26,9 +26,7 @@ from . import metrics
 __author__ = 'nnorwitz@google.com (Neal Norwitz)'
 
 
-def _find_warnings(filename, source, ast_list, static_is_optional):
-    lines = metrics.Metrics(source)
-
+def _find_warnings(filename, lines, ast_list, static_is_optional):
     def print_warning(node, name):
         print("{}:{}: Static data '{}'".format(
             filename,
@@ -40,41 +38,43 @@ def _find_warnings(filename, source, ast_list, static_is_optional):
             if node.name == 'static':
                 # TODO(nnorwitz): should ignore const. Is static const common
                 # here?
-                lines = metrics.Metrics(source)
                 print_warning(node, lines.get_line(node.start).strip())
 
-    static_declarations = {}
     for node in ast_list:
         if isinstance(node, ast.VariableDeclaration):
             # Ignore 'static' at module scope so we can find globals too.
             is_static = 'static' in node.type.modifiers
             is_not_const = 'const' not in node.type.modifiers
+
             if is_not_const and (static_is_optional or is_static):
                 print_warning(node, node.name)
-                static_declarations[node.name] = node
         elif isinstance(node, ast.Function):
             if node.body:
                 find_static(node)
         elif isinstance(node, ast.Class) and node.body:
-            _find_warnings(filename, source, node.body, False)
-
-    _find_unused_static_warnings(ast_list, static_declarations,
-                                 filename=filename,
-                                 lines=lines)
+            _find_warnings(filename, lines, node.body, False)
 
 
-def _find_unused_static_warnings(ast_list, static_declarations,
-                                 filename, lines):
+def _find_unused_static_warnings(filename, lines, ast_list):
     """Warn about unused static variables."""
-    static_counts = collections.Counter()
+    static_declarations = {}
+
+    for node in ast_list:
+        if (
+            isinstance(node, ast.VariableDeclaration) and
+            'static' in node.type.modifiers
+        ):
+            static_declarations[node.name] = node
+
+    static_use_counts = collections.Counter()
     for node in ast_list:
         if isinstance(node, ast.Function) and node.body:
             for child in node.body:
                 if child.name in static_declarations:
-                    static_counts[child.name] += 1
+                    static_use_counts[child.name] += 1
 
-    for name, count in static_counts.items():
-        if count == 1:
+    for name in static_declarations:
+        if not static_use_counts[name]:
             print("{}:{}: Unused variable '{}'".format(
                 filename,
                 lines.get_line_number(static_declarations[name].start),
@@ -82,4 +82,7 @@ def _find_unused_static_warnings(ast_list, static_declarations,
 
 
 def run(filename, source, entire_ast, include_paths):
-    _find_warnings(filename, source, entire_ast, True)
+    lines = metrics.Metrics(source)
+
+    _find_warnings(filename, lines, entire_ast, True)
+    _find_unused_static_warnings(filename, lines, entire_ast)
