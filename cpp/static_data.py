abb967c1c2c5bc0f9aca38f9ff6d2db9c47fac59
Renaud Durlin <renaud.durlin@gmail.com>
2015-11-07 09:40:52 +0100

    Sort unused static declaration warnings

--- cpp/static_data.py
+++ cpp/static_data.py
@@ -97,7 +97,8 @@ def _find_unused_static_warnings(filename, lines, ast_list):
                     find_variables_use(child.body)
 
     count = 0
-    for name in sorted(static_declarations):
+    for name,_ in sorted(static_declarations.items(),
+                         key=lambda (key,value): value.start):
         if not static_use_counts[name]:
             print("{}:{}: unused variable '{}'".format(
                 filename,
