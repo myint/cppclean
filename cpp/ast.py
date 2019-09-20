cdbc6889b4121facf31f03df2e16899c16a4bc93
TomerJLevy <TomerJlevy@gmail.com>
2019-09-20 11:30:01 +0300

    Skip ALL preprocessors tokens
    To avoid IndexError on tokens that has type PREPROCESSORS
    Fixing issue - Index out of range #140

--- cpp/ast.py
+++ cpp/ast.py
@@ -1091,7 +1091,7 @@ class ASTBuilder(object):
         if token.name == ':':
             while token.name != ';' and token.name != '{':
                 # skip preprocesors macros
-                if token.name.startswith(('#if', '#elif', '#else', '#endif')):
+                if token.token_type == tokenize.PREPROCESSOR:
                     token = self._get_next_token()
                     continue
                 member, token = self.get_name()
