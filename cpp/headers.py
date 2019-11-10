8340aba04801dace91ad8efeedf96370141f6600
yann-yy <yann12458@gmail.com>
2019-11-11 00:15:20 +0800

    fix false positive of iclude header path reletive to current path (#151)

--- cpp/headers.py
+++ cpp/headers.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Find and print the headers #include'd in a source file."""
 
 from __future__ import absolute_import
@@ -23,13 +22,14 @@ import os
 
 from . import utils
 
-
 __author__ = 'nnorwitz@google.com (Neal Norwitz)'
 
 
 def read_source(filename, include_paths):
     for path in include_paths:
         actual_filename = os.path.join(path, filename)
+        actual_filename = actual_filename if not actual_filename.startswith(
+            "./") else actual_filename[2:]
         source = utils.read_file(actual_filename, False)
         if source is not None:
             return source, actual_filename
