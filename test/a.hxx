ff8d958c14112027f11bf7543d78157f9a90b0cf
Deepak Yadav <yadavd@gmail.com>
2020-05-24 14:41:32 +0530

    Fix for include .hxx file #155

---
+++ test/a.hxx
@@ -0,0 +1,8 @@
+#ifndef _A_H_
+#define _A_H_
+
+#include "b.hxx"
+
+void myFunc(int a);
+
+#endif
---
+++ test/b.hxx
@@ -0,0 +1,14 @@
+#ifndef _B_H_
+#define _B_H_
+
+namespace Name1 {
+class ABC {
+public:
+  ABC(): int1(0) {}
+  int getInt() { return int1; }
+private:
+  int int1;
+};
+}
+
+#endif
